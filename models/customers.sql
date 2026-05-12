344851eded254c6b4b10efadd981353866faa1c7
leonhnoel <leonhekkert@protonmail.com>
2026-05-12 13:51:42 +0200

    fix: add sqlfluff configuration and ignore files

--- models/customers.sql
+++ models/customers.sql
@@ -1,56 +1,61 @@
-with customers as (
+WITH customers AS (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
+    SELECT
+        id AS customer_id
+        , first_name
+        , last_name
 
-    from raw.jaffle_shop.customers
-
-),
-
-orders as (
-
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from raw.jaffle_shop.orders
-
-),
-
-customer_orders as (
-
-    select
-        customer_id,
-
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-
-    from orders
-
-    group by 1
-
-),
-
-
-final as (
-
-    select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
-    from customers
-
-    left join customer_orders using (customer_id)
+    FROM
+        raw.jaffle_shop.customers
 
 )
 
-select * from final
+, orders AS (
+
+    SELECT
+        id AS order_id
+        , user_id AS customer_id
+        , order_date
+        , status
+
+    FROM
+        raw.jaffle_shop.orders
+
+)
+
+, customer_orders AS (
+
+    SELECT
+        customer_id
+        , min(order_date) AS first_order_date
+        , max(order_date) AS most_recent_order_date
+        , count(order_id) AS number_of_orders
+
+    FROM
+        orders
+
+    GROUP BY
+        1
+
+)
+
+, final AS (
+
+    SELECT
+        customers.customer_id
+        , customers.first_name
+        , customers.last_name
+        , customer_orders.first_order_date
+        , customer_orders.most_recent_order_date
+        , coalesce(customer_orders.number_of_orders, 0) AS number_of_orders
+
+    FROM
+        customers
+
+    LEFT JOIN
+        customer_orders
+        ON customers.customer_id = customer_orders.customer_id
+
+)
+
+SELECT * FROM final
